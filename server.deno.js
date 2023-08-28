4e9335f45b1863072872c492553c5e0cc3e67dc5
yamaji <haruyuki.program@gmail.com>
2023-08-28 19:29:01 +0900

    prettierの設定を更新・フォーマットを適用

--- server.deno.js
+++ server.deno.js
@@ -1,17 +1,17 @@
-import { serve } from "https://deno.land/std@0.151.0/http/server.ts";
-import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
+import { serve } from 'https://deno.land/std@0.151.0/http/server.ts';
+import { serveDir } from 'https://deno.land/std@0.151.0/http/file_server.ts';
 
 serve(async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
 
-  if (req.method === "GET" && pathname === "/welcome-message") {
-    return new Response("jigインターンへようこそ！");
+  if (req.method === 'GET' && pathname === '/welcome-message') {
+    return new Response('jigインターンへようこそ！');
   }
 
   return serveDir(req, {
-    fsRoot: "public",
-    urlRoot: "",
+    fsRoot: 'public',
+    urlRoot: '',
     showDirListing: true,
     enableCors: true,
   });
