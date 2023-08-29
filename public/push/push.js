f43c83c97cbb3d7c13e00d243fa671dcf344886a
Yushin Ito <75526539+yushin-ito@users.noreply.github.com>
2023-08-29 17:08:54 +0900

    Add: スタンプを押す画面のUIを作成

--- public/push/push.js
+++ public/push/push.js
@@ -1,3 +1,17 @@
 export const init = function () {
   console.log('push init');
+
+  document.querySelector('#picker').addEventListener('change', (event) => {
+    const file = event.target.files[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      document.querySelector('#img').src = event.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  });
 };
