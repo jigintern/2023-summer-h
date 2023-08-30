628473775a1c6a288f4f164617606d5e7d4815ce
aisuQwQ <aisuQwQ@ya.ru>
2023-08-30 23:24:50 +0900

    feat: スタンプ帳登録
    APIを叩けるように

--- public/popup/create-note/create-note.js
+++ public/popup/create-note/create-note.js
@@ -3,4 +3,28 @@ export const init = function () {
   createNotePop.addEventListener('click', (ev) => {
     ev.stopPropagation();
   });
+
+  //作成ボタン
+  const createNoteButton = document.querySelector('div.create-note a');
+  createNoteButton.addEventListener('click', async (ev)=>{
+
+    //スタンプ帳API叩く
+    const title=document.querySelector('div.create-note input').value;
+    const user_id=JSON.parse(window.sessionStorage.getItem("session")).user.id;
+    const data={
+      user_id:user_id, 
+      title:title
+    };
+    const params={
+      method:'POST', 
+      body: JSON.stringify(data)
+    };
+    const response=await fetch('/newnote', params);
+    const note_id=await response.text();
+    console.log(note_id);
+    //overlay削除
+    const overlay = document.querySelector('div.overlay');
+    overlay.style.display = null;
+    overlay.innerHTML = '';
+  });
 };
