7ceb179b1579c72f7e2fdfe1a3984744ea4e6403
haruyuki <haruyuki.program@gmail.com>
2023-08-30 11:10:05 +0900

    アカウント登録を実装

--- public/popup/account/account.js
+++ public/popup/account/account.js
@@ -1,6 +1,105 @@
+import { Base256B } from 'https://code4fukui.github.io/Base256B/Base256B.js';
+
 export const init = function () {
+  const overlay = document.querySelector('div.overlay');
+  const errorSpan = document.querySelector('span.error');
   const accountPop = document.querySelector('div.account');
   accountPop.addEventListener('click', (ev) => {
     ev.stopPropagation();
   });
+
+  const iconImageSelectButton = document.querySelector(
+    'button#iconImageSelect'
+  );
+  iconImageSelectButton.addEventListener('click', () => {
+    document.querySelector('input#icon').click();
+  });
+
+  document.querySelector('input#icon').addEventListener('change', (ev) => {
+    const [iconFile] = ev.target.files;
+
+    if (iconFile) {
+      iconImageSelectButton.style.backgroundImage = `url(${URL.createObjectURL(
+        iconFile
+      )})`;
+    }
+  });
+
+  document
+    .querySelector('button#register')
+    .addEventListener('click', async (ev) => {
+      ev.target.setAttribute('disabled', 'true');
+      const username = document.querySelector('input#username').value;
+      const email = document.querySelector('input#email').value;
+      let password = document.querySelector('input#password').value;
+
+      if (!username || !email || !password) {
+        errorSpan.style.display = 'block';
+        errorSpan.textContent = '未入力の項目があります';
+        return;
+      } else {
+        errorSpan.style.display = 'none';
+        errorSpan.textContent = '';
+      }
+
+      for (let i = 0; i < 10000; i++) {
+        password = await digest(password);
+      }
+
+      // ユーザー登録
+      const body = {
+        username: username,
+        email: email,
+        password: password,
+      };
+
+      let res;
+      const decoder = new TextDecoder();
+      res = await fetch('/users/register', {
+        method: 'POST',
+        body: JSON.stringify(body),
+      });
+
+      const reader = res.body.getReader();
+      let buf = '';
+      while (true) {
+        const tmp = await reader.read();
+        buf += decoder.decode(tmp?.value);
+        if (tmp.done) break;
+      }
+      if (res.status === 400) {
+        errorSpan.style.display = 'block';
+        errorSpan.textContent = buf;
+        return;
+      }
+      const data = JSON.parse(buf);
+      window.sessionStorage.setItem('session', JSON.stringify(data.session));
+      ev.target.removeAttribute('disabled');
+      overlay.style.display = null;
+      overlay.innerHTML = '';
+
+      // console.log(data);
+
+      // TODO: 実装
+      // // アイコン画像を登録
+      // res = await fetch(
+      //   `/users/icon?iconUrl=${data.user.user_metadata.iconurl}`,
+      //   {
+      //     method: 'POST',
+      //     body: document.querySelector('input#icon').files[0],
+      //   }
+      // );
+    });
+};
+
+// https://developer.mozilla.org/ja/docs/Web/API/SubtleCrypto/digest#%E3%83%80%E3%82%A4%E3%82%B8%E3%82%A7%E3%82%B9%E3%83%88%E5%80%A4%E3%82%92_16_%E9%80%B2%E6%96%87%E5%AD%97%E5%88%97%E3%81%AB%E5%A4%89%E6%8F%9B%E3%81%99%E3%82%8B
+const digest = async function (data) {
+  const encoder = new TextEncoder();
+  const encoded = encoder.encode(data);
+  const hash = await crypto.subtle.digest('SHA-256', encoded);
+  const hashArray = Array.from(new Uint8Array(hash));
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, '0'))
+    .join('');
+  return hashHex;
 };
--- server.deno.js
+++ server.deno.js
@@ -2,6 +2,7 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 import * as dotenv from 'https://deno.land/std@0.167.0/dotenv/mod.ts';
 import { serve } from 'https://deno.land/std@0.151.0/http/server.ts';
 import { serveDir } from 'https://deno.land/std@0.151.0/http/file_server.ts';
+import { Base256B } from 'https://code4fukui.github.io/Base256B/Base256B.js';
 
 await dotenv.config({
   export: true,
@@ -13,6 +14,8 @@ await dotenv.config({
 const config = Deno.env.toObject();
 
 const supabase = createClient(config['SUPABASE_URL'], config['SUPABASE_KEY']);
+
+const decoder = new TextDecoder();
 
 serve(async (req) => {
   const pathname = new URL(req.url).pathname;
@@ -29,33 +32,96 @@ serve(async (req) => {
 
   //スタンプ帳登録
   if (req.method === 'POST' && pathname === '/newnote') {
-    const json=await req.json();
-    const user_id=json.user_id;
-    const title=json.title;
-    const result=await supabase.from('notes').insert({user_id:user_id, title:title}).select();
+    const json = await req.json();
+    const user_id = json.user_id;
+    const title = json.title;
+    const result = await supabase
+      .from('notes')
+      .insert({ user_id: user_id, title: title })
+      .select();
     return new Response(result.data[0].id);
   }
 
   //スタンプ登録
   if (req.method === 'POST' && pathname === '/newstamp') {
-    const json=await req.json();
-    const note_id=json.note_id;
-    const title=json.title;
-    const landmark=json.landmark;
-    const category=json.category;
-    const latitude=json.latitude;
-    const longitude=json.longitude;
-    
-    const result=await supabase.from('stamps').insert({
-      note_id:note_id,
-      title:title, 
-      landmark:landmark,
-      category:category,
-      latitude:latitude,
-      longitude:longitude
-    }).select();
-    return new Response()
+    const json = await req.json();
+    const note_id = json.note_id;
+    const title = json.title;
+    const landmark = json.landmark;
+    const category = json.category;
+    const latitude = json.latitude;
+    const longitude = json.longitude;
+
+    const result = await supabase
+      .from('stamps')
+      .insert({
+        note_id: note_id,
+        title: title,
+        landmark: landmark,
+        category: category,
+        latitude: latitude,
+        longitude: longitude,
+      })
+      .select();
+    return new Response();
   }
+
+  if (req.method === 'POST' && pathname === '/users/register') {
+    const reader = req.body?.getReader();
+    let buf = '';
+    while (true) {
+      const tmp = await reader.read();
+      buf += decoder.decode(tmp?.value);
+      if (tmp.done) break;
+    }
+    const body = JSON.parse(buf);
+
+    const iconUrl = `${crypto.randomUUID()}.png`;
+
+    const res = await supabase.auth.signUp({
+      email: body.email,
+      password: body.password,
+      options: {
+        data: {
+          username: body.username,
+          iconurl: iconUrl,
+        },
+      },
+    });
+
+    if (res.error) return new Response(res.error, { status: 400 });
+    return new Response(JSON.stringify(res.data), { status: 201 });
+  }
+
+  // TODO: 実装
+  // if (pathname === '/users/icon') {
+  //   const iconUrl = new URL(req.url).searchParams.get('iconUrl');
+  //   console.log(iconUrl);
+  //   if (req.method === 'POST') {
+  //     const reader = req.body?.getReader();
+  //     /**
+  //      * @type {Uint8Array}
+  //      */
+  //     const buf = new Uint8Array();
+  //     while (true) {
+  //       const tmp = await reader.read();
+  //       buf.join(tmp?.value);
+  //       if (tmp.done) break;
+  //     }
+  //     console.log(buf);
+  //     // const body = JSON.parse(buf);
+  //     const image = new Blob([buf.buffer], { type: 'image/png' } /* (1) */);
+  //     const res = await supabase.storage.from('icons').upload(iconUrl, image);
+
+  //     console.log(res);
+
+  //     if (res.error) {
+  //       return new Response(res.error, { status: 400 });
+  //     } else {
+  //       return new Response('success', { status: 201 });
+  //     }
+  //   }
+  // }
 
   return serveDir(req, {
     fsRoot: 'public',
