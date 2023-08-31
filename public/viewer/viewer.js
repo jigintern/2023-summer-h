cf7373a3fa8c13e3109aed40e9b7152769f612a2
Yushin Ito <75526539+yushin-ito@users.noreply.github.com>
2023-08-31 19:47:22 +0900

    Add: 自分のスタンプ帳のスタンプを表示するモーダルを実装

--- public/viewer/viewer.js
+++ public/viewer/viewer.js
@@ -1,3 +1,50 @@
+import { createNoteCard } from '../components/card.js';
+import * as myNote from '../modal/my-note/my-note.js';
+
 export const init = function () {
-  console.log('viewer init');
+  const overlay = document.querySelector('div.overlay');
+  const grid = document.querySelector('div.grid');
+
+  const dummyNotes = [
+    {
+      note_id: 0,
+      title: '奈良旅行１日目',
+      url: null,
+      createdAt: '2022/01/10',
+    },
+    {
+      note_id: 1,
+      title: '奈良旅行2日目',
+      url: null,
+      createdAt: '2022/01/11',
+    },
+    {
+      note_id: 2,
+      title: '奈良旅行3日目',
+      url: null,
+      createdAt: '2022/01/12',
+    },
+    {
+      note_id: 3,
+      title: '奈良旅行4日目',
+      url: null,
+      createdAt: '2022/01/13',
+    },
+  ];
+
+  dummyNotes.forEach((item) => {
+    const noteCard = createNoteCard(
+      item.url,
+      item.title,
+      item.createdAt,
+      async () => {
+        overlay.style.display = 'grid';
+        overlay.innerHTML = await (
+          await fetch('./modal/my-note/my-note.html')
+        ).text();
+        myNote.init();
+      }
+    );
+    grid.appendChild(noteCard);
+  });
 };
