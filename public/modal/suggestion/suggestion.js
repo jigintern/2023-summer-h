bf62a2fcf28d050a244b2600884bee6453e029ca
Yushin Ito <75526539+yushin-ito@users.noreply.github.com>
2023-08-31 16:09:17 +0900

    Add: スタンプ帳を開くためにモーダルが拡張するようにした

--- public/modal/suggestion/suggestion.js
+++ public/modal/suggestion/suggestion.js
@@ -11,4 +11,15 @@ export const show = async function () {
   });
   await delay(0.01);
   suggestion.classList.add('active');
+
+  const note = document.getElementsByTagName('li');
+  for (const stamp of note) {
+    stamp.addEventListener('click', () => {
+      suggestion.classList.add('open');
+      document.querySelector('div.title').innerHTML =
+        '<h3>京都旅行1日目</h3>';
+    });
+      <ul class="scroll-box"></ul>
+
+  }
 };
